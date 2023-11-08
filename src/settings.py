71baa6b3db0ce47da41cb2d1f86c848a90bff62a
erastov-alex <erastov.alex@gmail.com>
2023-11-08 21:58:46 +0300

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Digital Breakthrough 2022: Car Plate"
 PROJECT_NAME_FULL: str = "Digital Breakthrough 2022: Ulyanovsk - Car Plate Detection"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
